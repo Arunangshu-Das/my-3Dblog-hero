04a89cded3cc03359afabca7b224df3c8a25886d
Arunangshu-Das <darunangshu2002@gmail.com>
2023-07-18 11:04:58 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,325 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  carrent,
+  threejs,
+  youtube,
+  linkedin,
+  github,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Services",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Web Developer",
+    icon: web,
+  },
+  {
+    title: "Machine learning Developer",
+    icon: backend,
+  },
+  {
+    title: "Spring Boot Developer",
+    icon: creator,
+  },
+  {
+    title: "Blockchain Developer",
+    icon: mobile,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "Spring",
+    icon: creator,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "ML",
+    icon: backend,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "blockchain",
+    icon: mobile,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Tech consultancy",
+    company_name: "01",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611627/techconsultancy_zdqjfn.jpg",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [
+      "Embark on a seamless product development journey or startup launch by leveraging my expertise for frictionless progress. With my wealth of experience, we'll navigate the development process effortlessly, ensuring a smooth path to success.",
+    ],
+  },
+  {
+    title: "Event Talks",
+    company_name: "02",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/techtalk_phk1r0.jpg",
+    iconBg: "#E6DEDD",
+    date: "--->",
+    points: [
+      "Having spoken at over 100+ conferences globally, I can undoubtedly bring tremendous attention to your next event, ensuring it's an exceptional success.",
+    ],
+  },
+  {
+    title: "Social Media live",
+    company_name: "03",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611625/live_a6cgpb.png",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [
+      "Catch me live on social media and podcasts, but remember, availability is limited! Secure your spot in advance, and let's connect for engaging discussions on all things tech and beyond. Don't miss out on the chance to be part of these exciting events!",
+    ],
+  },
+  {
+    title: "Freelancing",
+    company_name: "04",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/free_t37ytb.png",
+    iconBg: "#E6DEDD",
+    date: "--->",
+    points: [
+      "Unlock your full potential in the freelance world! Whether you're a writer, designer, developer, or any creative genius, we've got you covered. Elevate your freelancing career with our tailored strategies, expanding your reach on platforms like Upwork, Fiverr, and more. Maximize exposure, land high-paying gigs, and build a strong brand presence through our expert promotion and engagement techniques. Together, let's make your freelance journey shine!",
+    ],
+  },
+  {
+    title: "Promotion",
+    company_name: "05",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/promo_wxuq81.png",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [
+      "Introducing a new product, app, or game? Let's explore how we can elevate your launch to the perfect spotlight through captivating YouTube and Instagram strategies. Get ready to shine and make a lasting impact on your target audience with our expert promotion and engagement techniques.",
+    ],
+  },
+];
+
+const experiences1 = [
+  {
+    title: "Software Development & Algorithm",
+    company_name: "01",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/soft_dpebvi.png",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [""],
+  },
+  {
+    title: "Machine Learning",
+    company_name: "02",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/ml_zxb6m4.jpg",
+    iconBg: "#E6DEDD",
+    date: "--->",
+    points: [""],
+  },
+  {
+    title: "Spring Boot",
+    company_name: "03",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/spring_cuieu3.png",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [""],
+  },
+  {
+    title: "Image Processing",
+    company_name: "04",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/imgprocess_pjnjy5.jpg",
+    iconBg: "#E6DEDD",
+    date: "--->",
+    points: [""],
+  },
+  {
+    title: "NLP",
+    company_name: "05",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/nlp_yfuit5.jpg",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [""],
+  },
+  {
+    title: "Blockchain",
+    company_name: "06",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611625/blockchain_sbib8n.jpg",
+    iconBg: "#E6DEDD",
+    date: "<---",
+    points: [""],
+  },
+  {
+    title: "CS Fundamental",
+    company_name: "07",
+    icon: "https://res.cloudinary.com/dpmu1oy8z/image/upload/v1689611626/cs_nlbg4b.png",
+    iconBg: "#f70c4a",
+    date: "<---",
+    points: [""],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Youtube",
+    description: "Code & Arunangshu",
+    icon: youtube,
+    tags: [
+      {
+        name: "Code",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Algo",
+        color: "green-text-gradient",
+      },
+      {
+        name: "TechConsultancy",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://www.youtube.com/@codingwitharunangshu",
+  },
+  {
+    name: "Linkedin",
+    description: "Arunangshu Das",
+    icon: linkedin,
+    tags: [
+      {
+        name: "Code",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "DeepLearning",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tech",
+        color: "pink-text-gradient",
+      },
+    ],
+    image:
+      "https://content.linkedin.com/content/dam/brand/site/img/logo/logo-hero.png",
+    source_code_link: "https://www.linkedin.com/in/arunangshu-das/",
+  },
+  {
+    name: "GitHub",
+    description: "Arunangshu Das",
+    icon: github,
+    tags: [
+      {
+        name: "Code",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Springboot",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tech",
+        color: "pink-text-gradient",
+      },
+    ],
+    image:
+      "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+    source_code_link: "https://github.com/Arunangshu-Das",
+  },
+];
+
+export {
+  services,
+  technologies,
+  experiences,
+  testimonials,
+  projects,
+  experiences1,
+};
